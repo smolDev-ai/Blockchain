40757259d7ab2416e6d5a456538bdf21ab97f78a
smolDev-ai <driahthedragonwalker@gmail.com>
2020-03-11 16:32:29 -0500

    created the routes, and updated miner to pull in the data properly and keep track of coins

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -1,2 +1,112 @@
-# Paste your version of blockchain.py from the basic_block_gp
-# folder here
+import hashlib
+import json
+from time import time
+from uuid import uuid4
+
+from flask import Flask, jsonify, request
+
+DIFFICULTY = "000000"
+
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, previous_hash, proof):
+        block = {
+            "index": len(self.chain) + 1,
+            "timestamp": time(),
+            "transactions": self.current_transactions,
+            "proof": proof,
+            "previous_hash": previous_hash or self.hash(self.last_block)
+
+        }
+
+        self.current_transactions = []
+
+        self.chain.append(block)
+
+        return block
+
+    def hash(self, block):
+        new_string = json.dumps(block, sort_keys=True)
+        block_string = new_string.encode()
+
+        hash_string = hashlib.sha256(block_string).hexdigest()
+
+        return hash_string
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+
+    @staticmethod
+    def valid_proof(block_string, proof):
+        guess = f"{block_string}{proof}".encode()
+        guessing_hash = hashlib.sha256(guess).hexdigest()
+        return guessing_hash[:6] == DIFFICULTY
+
+app = Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+
+@app.route('/')
+def api_up():
+    return "Api is up!"
+
+
+@app.route('/chain')
+def full_chain():
+    response = {
+        "chain": blockchain.chain,
+        "length": len(blockchain.chain),
+    }
+
+    return jsonify(response), 200
+
+
+@app.route('/last_block', methods=['GET'])
+def get_last_block():
+    response = {
+        "chain": blockchain.last_block,
+        "length": len(blockchain.chain)
+    }
+
+    return jsonify(response), 200
+
+
+@app.route('/mine', methods=['POST'])
+def mine():
+    data = request.get_json()
+    print(f"data: {data}")
+    
+    if data["proof"] is None or data["id"] is None:
+        response = {
+            "message": "INVALID, proof and/or id must be supplied!"
+        }
+        return jsonify(response), 400
+    
+    proof = data["proof"]
+    block_string = json.dumps(blockchain.last_block, sort_keys=True)
+    new_hash = blockchain.valid_proof(block_string, proof)
+
+    if new_hash is True:
+        if blockchain.last_block["proof"] != proof:
+            new_block = blockchain.new_block(proof, blockchain.last_block['previous_hash'])
+            
+            response = {
+                "message": "New Block Forged",
+                "new_block": new_block,
+            }
+            
+            return jsonify(response), 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -4,16 +4,15 @@ import requests
 import sys
 import json
 
+from blockchain import DIFFICULTY
+
 
 def proof_of_work(block):
-    """
-    Simple Proof of Work Algorithm
-    Stringify the block and look for a proof.
-    Loop through possibilities, checking each one against `valid_proof`
-    in an effort to find a number that is a valid proof
-    :return: A valid proof for the provided block
-    """
-    pass
+    proof = 0
+    string_object = json.dumps(block, sort_keys=True)
+    while valid_proof(string_object, proof) is False:
+        proof += 1
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,9 +26,11 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f"{block_string}{proof}".encode()
+    guessing_hash = hashlib.sha256(guess).hexdigest()
+    return guessing_hash[:6] == DIFFICULTY
 
-
+coins = 0
 if __name__ == '__main__':
     # What is the server address? IE `python3 miner.py https://server.com/api/`
     if len(sys.argv) > 1:
@@ -56,7 +57,7 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        new_proof = proof_of_work(data['chain'])
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
@@ -67,4 +68,10 @@ if __name__ == '__main__':
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        
+        if data["message"] == "New Block Forged":
+            coins += 1
+            print(f"coins mined: {coins}")
+        else:
+            print(f"{data['message']}")
+
